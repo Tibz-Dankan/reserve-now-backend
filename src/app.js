b3091dfb38d1d69cda6b943619b1dbf56438b554
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-09 20:26:42 +0300

    Server up and running

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-// Main file of the application
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT + " 🚀");
+});
