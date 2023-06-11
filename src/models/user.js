aaafcdc372d0a4f163ed7e4b4f85e7281ff72805
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-11 20:29:54 +0300

    feat: add user model

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const POSTGRES_URI = process.env.POSTGRES_URI;
+const sequelize = new Sequelize(POSTGRES_URI);
+
+const Users = sequelize.define("Users", {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM("user", "client", "staff", "admin"),
+    defaultValue: "user",
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+});
+
+// TODO: include virtuals, getters and setters
+
+module.exports = Users;
