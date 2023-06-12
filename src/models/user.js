159786f2b2ba83d3f32e52b79a79e04c70a265be
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-12 19:45:54 +0300

    feat: add .sequelizerc for dynamic sequelize configurations

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const POSTGRES_URI = process.env.POSTGRES_URI;
 const sequelize = new Sequelize(POSTGRES_URI);
