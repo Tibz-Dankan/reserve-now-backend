1b2b085b128c217e890156f2a1f5eef00a212abd
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-17 02:13:40 +0300

    test: forgot-password end point tests

---
+++ src/routes/auth/__test__/forgotPassword.test.js
@@ -0,0 +1,38 @@
+const request = require("supertest");
+const { app } = require("../../../app");
+
+describe("test the forgot-password api end point", () => {
+  it("returns a 400 with missing email", async () => {
+    await request(app)
+      .post("/v1/api/users/forgot-password")
+      .send({ email: "" })
+      .expect(400);
+  });
+
+  it("returns a 404 with invalid email", async () => {
+    await request(app)
+      .post("/v1/api/users/forgot-password")
+      .send({
+        email: "invalid@test.com",
+      })
+      .expect(404);
+  });
+
+  it("returns a 200 with valid email", async () => {
+    await request(app)
+      .post("/v1/api/users/signup")
+      .send({
+        name: "test user",
+        email: "test@test.com",
+        password: "password",
+        country: "country",
+      })
+      .expect(201);
+    await request(app)
+      .post("/v1/api/users/forgot-password")
+      .send({
+        email: "test@test.com",
+      })
+      .expect(200);
+  });
+});
--- src/routes/auth/__test__/signin.test.js
+++ src/routes/auth/__test__/signin.test.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
 const { app } = require("../../../app");
-const db = require("../../../models");
 
 describe("test the signin api end point", () => {
   it("returns a 200 on successful signin", async () => {
