af03569b8443f805cef3fb05510bb85289ade56a
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-17 02:03:42 +0300

    test(fix): correct signup api end point test

--- src/routes/auth/__test__/signup.test.js
+++ src/routes/auth/__test__/signup.test.js
@@ -4,7 +4,7 @@ const { app } = require("../../../app");
 describe("test signup api end point", () => {
   it("returns a 201 on successful signup", async () => {
     return request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
         name: "test user",
         email: "test@test.com",
@@ -16,68 +16,75 @@ describe("test signup api end point", () => {
 
   it("returns a 400 with an invalid email", async () => {
     return request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
+        name: "test user",
         email: "test.com",
         password: "password",
+        country: "country",
       })
       .expect(400);
   });
 
-  it("returns a 400 with missing username or email or password", async () => {
+  it("returns a 400 with missing name or email or password", async () => {
     await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "",
-        email: "email@test.com",
+        name: "",
+        email: "test@test.com",
         password: "password",
+        country: "country",
       })
       .expect(400);
     await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
+        name: "test user",
         email: "",
         password: "password",
+        country: "country",
       })
       .expect(400);
     await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
-        email: "test@gmail.com",
+        name: "test user",
+        email: "test@test.com",
         password: "",
+        country: "country",
       })
       .expect(400);
   });
 
   it("disallows duplicate emails", async () => {
     await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
+        name: "test user",
         email: "test@test.com",
         password: "password",
+        country: "password",
       })
       .expect(201);
     await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
+        name: "test user",
         email: "test@test.com",
         password: "password",
+        country: "country",
       })
       .expect(400);
   });
 
   it("expects a token after signup", async () => {
     const res = await request(app)
-      .post("/api/users/signup")
+      .post("/v1/api/users/signup")
       .send({
-        username: "test user",
+        name: "test user",
         email: "test@test.com",
         password: "password",
+        country: "password",
       })
       .expect(201);
 
