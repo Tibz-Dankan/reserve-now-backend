b294d2c215d0311007d569ca763073db1b244b9a
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-19 09:21:27 +0300

    feat: functionality to allow upload file

---
+++ src/utils/upload.js
@@ -0,0 +1,110 @@
+const { initializeApp } = require("firebase/app");
+const { deleteObject, uploadString } = require("firebase/storage");
+const { getStorage, ref, getDownloadURL } = require("firebase/storage");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const { AppError } = require("./error");
+
+dotenv.config();
+
+const multerConfig = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+});
+const uploadFile = multerConfig.single("file");
+
+class Upload {
+  firebaseStorage;
+  filePath;
+  upload;
+  next;
+
+  constructor(filePath, next) {
+    const firebaseConfig = {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    };
+    const firebaseApp = initializeApp(firebaseConfig);
+    this.firebaseStorage = getStorage(firebaseApp);
+    this.filePath = filePath;
+    this.upload = { url: "" };
+    this.next = next;
+  }
+
+  bufferToBase64(buffer) {
+    return Buffer.from(buffer).toString("base64");
+  }
+
+  path(filePath) {
+    const isProduction = process.env.NODE_ENV === "production";
+    if (!filePath) {
+      this.next(new AppError("No file path provided!", 500));
+    }
+    if (filePath.startsWith("/")) {
+      this.next(new AppError("Invalid file path!", 500));
+    }
+    if (isProduction) return `prod/${filePath}`;
+    if (!isProduction) return `dev/${filePath}`;
+  }
+
+  async add(file) {
+    try {
+      const reference = ref(this.firebaseStorage, this.path(this.filePath));
+      const fileBase64 = this.bufferToBase64(file.buffer);
+
+      await uploadString(reference, fileBase64, "base64");
+      this.upload.url = await getDownloadURL(reference);
+
+      return this.upload;
+    } catch (err) {
+      console.log("err", err);
+      if (err)
+        return this.next(
+          new AppError("Sorry, error occurred while uploading!", 500)
+        );
+    }
+  }
+
+  async update(file, savedFilePath) {
+    try {
+      let reference = ref(this.firebaseStorage, this.path(this.filePath));
+      const fileBase64 = this.bufferToBase64(file.buffer);
+      await uploadString(reference, fileBase64, "base64");
+      const URL = await getDownloadURL(reference);
+
+      // delete the saved file
+      reference = ref(this.firebaseStorage, this.path(savedFilePath));
+      if (URL) {
+        await deleteObject(reference);
+      }
+      this.upload.url = URL;
+
+      return this.upload;
+    } catch (err) {
+      console.log("err", err);
+      if (err)
+        return this.next(
+          new AppError("Sorry, error occurred while uploading!", 500)
+        );
+    }
+  }
+
+  async delete() {
+    try {
+      const reference = ref(this.firebaseStorage, this.path(this.filePath));
+      await deleteObject(reference);
+    } catch (err) {
+      console.log("err", err);
+      this.next(new AppError("Sorry, error occurred while deleting!", 500));
+    }
+  }
+}
+
+module.exports = { Upload, uploadFile };
