27e9043ebf35cd26e0da8d506028c24a6759a64f
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-19 12:44:24 +0300

    test(addRom): add tests for add-room api end point

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -17,7 +17,7 @@ const addRoom = asyncHandler(async (req, res, next) => {
 
   const newRoom = await Room.create(req.body);
 
-  res.status(200).json({ success: true, data: newRoom });
+  res.status(201).json({ success: true, data: newRoom });
 });
 
 const getAllRooms = asyncHandler(async (req, res, next) => {
---
+++ src/routes/room/__test__/addRoom.test.js
@@ -0,0 +1,137 @@
+const request = require("supertest");
+const { app } = require("../../../app");
+
+describe("test addRoom API endpoint", () => {
+  it("returns a 201 on successful room creation", async () => {
+    // signup to get token
+    const res = await request(app)
+      .post("/v1/api/users/signup")
+      .send({
+        name: "test user",
+        email: "test@test.com",
+        password: "password",
+        country: "country",
+      })
+      .expect(201);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "101",
+        roomType: "Standard",
+        capacity: 2,
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(201);
+  });
+
+  it("returns a 400 with missing roomNumber||roomType||capacity||price||priceCurrency ", async () => {
+    // signup to get token
+    const res = await request(app)
+      .post("/v1/api/users/signup")
+      .send({
+        name: "test user",
+        email: "test@test.com",
+        password: "password",
+        country: "country",
+      })
+      .expect(201);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "",
+        roomType: "Standard",
+        capacity: 2,
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "1",
+        roomType: "",
+        capacity: 2,
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "2",
+        roomType: "Standard",
+        capacity: "",
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "2",
+        roomType: "Standard",
+        capacity: "3",
+        price: "",
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "2",
+        roomType: "Standard",
+        capacity: "2",
+        price: 100,
+        priceCurrency: "",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+  });
+
+  it("disallows duplicate room numbers", async () => {
+    const res = await request(app)
+      .post("/v1/api/users/signup")
+      .send({
+        name: "test user",
+        email: "test@test.com",
+        password: "password",
+        country: "country",
+      })
+      .expect(201);
+
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "101",
+        roomType: "Standard",
+        capacity: 2,
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(201);
+    await request(app)
+      .post("/v1/api/rooms/add-room")
+      .send({
+        roomNumber: "101",
+        roomType: "Standard",
+        capacity: 2,
+        price: 100,
+        priceCurrency: "USD",
+      })
+      .set("Authorization", `Bearer ${res.body.token}`)
+      .expect(400);
+  });
+});
