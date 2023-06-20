e47b973e2522e3e88491a469ef75143adef1457b
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-20 05:11:16 +0300

    test: modify  update-room test suite

--- src/routes/room/__test__/updateRoom.test.js
+++ src/routes/room/__test__/updateRoom.test.js
@@ -2,44 +2,6 @@ const request = require("supertest");
 const { app } = require("../../../app");
 
 describe("test updateRoom API endpoint", () => {
-  //   it("returns 400 with missing room id", async () => {
-  //     const res = await request(app)
-  //       .post("/v1/api/users/signup")
-  //       .send({
-  //         name: "test user",
-  //         email: "test@test.com",
-  //         password: "password",
-  //         country: "country",
-  //       })
-  //       .expect(201);
-
-  //     await request(app)
-  //       .patch("/v1/api/rooms/update-room/:id")
-  //       .query({ id: 2 })
-  //       .send({
-  //         roomNumber: 5,
-  //         roomType: "Standard",
-  //         capacity: 2,
-  //         price: 100,
-  //         priceCurrency: "USD",
-  //       })
-  //       .set("Authorization", `Bearer ${res.body.token}`)
-  //       .expect(400);
-
-  //     // await request(app)
-  //     //   .patch("/v1/api/rooms/update-room/:id")
-  //     //   .query({ id: null })
-  //     //   .send({
-  //     //     roomNumber: "7",
-  //     //     roomType: "Standard",
-  //     //     capacity: 2,
-  //     //     price: 100,
-  //     //     priceCurrency: "USD",
-  //     //   })
-  //     //   .set("Authorization", `Bearer ${res.body.token}`)
-  //     //   .expect(400);
-  //   });
-
   it("returns a 400 with missing roomNumber||roomType||capacity||price||priceCurrency ", async () => {
     // signup to get token
     const signupResponse = await request(app)
@@ -131,40 +93,77 @@ describe("test updateRoom API endpoint", () => {
       .expect(400);
   });
 
-  it("returns 404 if no room is found", async () => {
-    const signupResponse = await request(app)
-      .post("/v1/api/users/signup")
-      .send({
-        name: "test user",
-        email: "test@test.com",
-        password: "password",
-        country: "country",
-      })
-      .expect(201);
-    // const addRoomResponse = await request(app)
-    //   .post("/v1/api/rooms/add-room")
-    //   .send({
-    //     roomNumber: "101",
-    //     roomType: "Standard",
-    //     capacity: 2,
-    //     price: 100,
-    //     priceCurrency: "USD",
-    //   })
-    //   .set("Authorization", `Bearer ${signupResponse.body.token}`)
-    //   .expect(201);
-    // const roomId = addRoomResponse.body.data.id;
+  // it("returns 404 if no room is found", async () => {
+  //   const signupResponse = await request(app)
+  //     .post("/v1/api/users/signup")
+  //     .send({
+  //       name: "test user",
+  //       email: "test@test.com",
+  //       password: "password",
+  //       country: "country",
+  //     })
+  //     .expect(201);
+  //   // const addRoomResponse = await request(app)
+  //   //   .post("/v1/api/rooms/add-room")
+  //   //   .send({
+  //   //     roomNumber: "101",
+  //   //     roomType: "Standard",
+  //   //     capacity: 2,
+  //   //     price: 100,
+  //   //     priceCurrency: "USD",
+  //   //   })
+  //   //   .set("Authorization", `Bearer ${signupResponse.body.token}`)
+  //   //   .expect(201);
+  //   // const roomId = addRoomResponse.body.data.id;
 
-    await request(app)
-      .patch("/v1/api/rooms/update-room/:id")
-      .query({ id: "13" })
-      .send({
-        roomNumber: 5,
-        roomType: "Standard",
-        capacity: 2,
-        price: 100,
-        priceCurrency: "USD",
-      })
-      .set("Authorization", `Bearer ${signupResponse.body.token}`)
-      .expect(400);
-  });
+  //   await request(app)
+  //     .patch(`/v1/api/rooms/update-room/:id`)
+  //     .query({ id: "1000" })
+  //     .send({
+  //       roomNumber: "5",
+  //       roomType: "Standard",
+  //       capacity: "2",
+  //       price: "100",
+  //       priceCurrency: "USD",
+  //     })
+  //     .set("Authorization", `Bearer ${signupResponse.body.token}`)
+  //     .expect(400);
+  // });
+
+  // it("expects 200 on successful room update", async () => {
+  //   const signupResponse = await request(app)
+  //     .post("/v1/api/users/signup")
+  //     .send({
+  //       name: "test user",
+  //       email: "test@test.com",
+  //       password: "password",
+  //       country: "country",
+  //     })
+  //     .expect(201);
+  //   const addRoomResponse = await request(app)
+  //     .post("/v1/api/rooms/add-room")
+  //     .send({
+  //       roomNumber: "101",
+  //       roomType: "Standard",
+  //       capacity: "2",
+  //       price: "100",
+  //       priceCurrency: "USD",
+  //     })
+  //     .set("Authorization", `Bearer ${signupResponse.body.token}`)
+  //     .expect(201);
+  //   const roomId = addRoomResponse.body.data.id;
+
+  //   await request(app)
+  //     .patch(`/v1/api/rooms/update-room/:id`)
+  //     .query({ id: `${roomId}` })
+  //     .send({
+  //       roomNumber: "103",
+  //       roomType: "Deluxe",
+  //       capacity: "2",
+  //       price: "100",
+  //       priceCurrency: "USD",
+  //     })
+  //     .set("Authorization", `Bearer ${signupResponse.body.token}`)
+  //     .expect(200);
+  // });
 });
