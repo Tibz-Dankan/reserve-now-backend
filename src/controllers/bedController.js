05d571e8460eeba09d41d327b98da7febf127058
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-08-24 17:03:18 +0300

    feat: update beds api endpoint

--- src/controllers/bedController.js
+++ src/controllers/bedController.js
@@ -1,4 +1,6 @@
 const Bed = require("../models").Bed;
+const Room = require("../models").Room;
+
 const { AppError } = require("../utils/error");
 const { asyncHandler } = require("../utils/asyncHandler");
 
@@ -32,6 +34,54 @@ const addBeds = asyncHandler(async (req, res, next) => {
   });
 });
 
+const updateBeds = asyncHandler(async (req, res, next) => {
+  const roomId = req.body.roomId;
+  const bedTypes = req.body.bedTypes;
+
+  if (!roomId) return next(new AppError("Please a roomId", 400));
+  if (!bedTypes[0]) {
+    return next(new AppError("Please provide at least one bed", 400));
+  }
+  // Delete all beds belonging to a room
+  await Bed.destroy({
+    where: {
+      roomId: roomId,
+    },
+  });
+
+  // save the new beds
+  bedTypes.map(async (bedType) => {
+    if (!bedType) {
+      return next(
+        new AppError(`Please provide value for bedType ${bedType}`, 400)
+      );
+    }
+    await Bed.create({
+      roomId: roomId,
+      bedType: bedType,
+    });
+  });
+
+  const updatedRoom = await Room.findOne({
+    where: { id: roomId },
+    include: [
+      {
+        model: Bed,
+        as: "beds",
+      },
+    ],
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "Beds updated successfully",
+    data: {
+      room: updatedRoom,
+    },
+  });
+});
+
 module.exports = {
   addBeds,
+  updateBeds,
 };
--- src/routes/bed/bedRoutes.js
+++ src/routes/bed/bedRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addBeds } = require("../../controllers/bedController");
+const { addBeds, updateBeds } = require("../../controllers/bedController");
 const { protect } = require("../../controllers/userController");
 
 const router = express.Router();
 
 router.post("/add-bed", protect, addBeds);
+router.patch("/update-bed", protect, updateBeds);
 
 const bedRoutes = router;
 module.exports = { bedRoutes };
